ec45c9b4596076ffb16b3aab4f79290d12e1553b
asathiya007 <akshay.sathiya@gmail.com>
2020-08-26 13:27:31 -0700

    added user modes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
 
 function App() {
+
+  const [mode, setMode] = useState("user"); 
+
+  const showContent = () => {
+    if (mode === "user") {
+      return <div>User - can view posts</div>; 
+    } 
+    return <div>Admin - can view and make posts</div>; 
+  }
+
   return (
     <div>
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand>Blog Site</Navbar.Brand>
         <Nav className="ml-auto">
-          <Nav.Link>Posts</Nav.Link>
-          <Nav.Link>Admin</Nav.Link>
+          <Nav.Link onClick={() => setMode("user")}>Posts</Nav.Link>
+          <Nav.Link onClick={() => setMode("admin")}>Admin</Nav.Link>
         </Nav>
       </Navbar>
+      {showContent()}
     </div>
   );
 }
