67ad1553fb7ca7b9d2ecc0e3ba9dceb915063110
asathiya007 <akshay.sathiya@gmail.com>
2020-08-26 13:16:13 -0700

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Navbar, Nav} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>Blog Site</Navbar.Brand>
+        <Nav className="ml-auto">
+          <Nav.Link>Posts</Nav.Link>
+          <Nav.Link>Admin</Nav.Link>
+        </Nav>
+      </Navbar>
     </div>
   );
 }
